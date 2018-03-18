2d2dd8c8e207ce2eb0dba3b43b7d2940606f1ae6
Androu <15367779+AndreasRapatas@users.noreply.github.com>
2018-03-18 15:24:05 +0200

    Added test for find_char_positions

--- tests/basic_testing.cpp
+++ tests/basic_testing.cpp
@@ -248,3 +248,26 @@ SCENARIO("next char") {
 		}
 	}
 }
+
+SCENARIO("positioning") {
+
+	GIVEN("an array 'subjective', 'subversive'") {
+
+		std::vector<std::string> dictionary {
+			"subjective",
+			"subversive"
+		};
+
+		WHEN ("find_char_positions(e)") {
+
+			auto ps = find_char_positions(dictionary, 'e');
+
+			THEN ("expect {4, 9}") {
+
+				REQUIRE(ps.value().size() == 2);
+				REQUIRE(ps.value()[0] == 4);
+				REQUIRE(ps.value()[1] == 9);
+			}
+		}
+	}
+}
